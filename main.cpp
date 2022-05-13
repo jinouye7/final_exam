c906f05db6bf2a98655fcb50f3468e9c89b6e169
jinouye7 <jinouye7@hawaii.edu>
2022-05-13 00:50:27 -1000

    initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  final_exam - EE 205 - Spr 2022
+///
+/// @file main.cpp
+/// @version 1.0
+///
+/// @author Jared Inouye <jinouye7@hawaii.edu>
+/// @date   13_May_2022
+///////////////////////////////////////////////////////////////////////////////
+#include "stdio.h"
+#include "stdlib.h"
+
+int main() {
+    printf("hello world!");
+};
